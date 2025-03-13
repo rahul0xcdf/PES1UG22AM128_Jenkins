d0e7d40fab469e1fb3268e3669bf718aa79fd569
Rahul <67908178+rahul0xcdf@users.noreply.github.com>
2025-03-13 15:53:53 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello from Jenkins Pipeline!   By PES1UG22AM128 "  << std::endl;
+    std::cout << "This is a test C++ program for the Jenkins assignment." << std::endl;
+    return 0;
+}
